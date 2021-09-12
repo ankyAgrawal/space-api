3e84a611c734a0e18b56ef4469c2d219a8e9cb5c
Ndagi Stanley <ndagis@gmail.com>
2021-09-12 19:27:19 +0300

    Add supertest and first test

--- tests/space.spec.js
+++ tests/space.spec.js
@@ -1,5 +1,19 @@
+const request = require('supertest');
+const app = require("../app");
+
 describe('Space test suite', () => {
-    it('My Space Test', () => {
-        expect(true).toEqual(true);
+    it('tests /destinations endpoints', async () => {
+        const response = await request(app).get("/space/destinations");
+        expect(response.body).toEqual(["Mars", "Moon", "Earth", "Mercury", "Venus", "Jupiter"]);
+        expect(response.body).toHaveLength(6);
+        expect(response.statusCode).toBe(200);
+        // Testing a single element in the array
+        expect(response.body).toEqual(expect.arrayContaining(['Earth']));
+
     });
+
+    // Insert other tests below this line
+
+    // Insert other tests above this line
+
 });
